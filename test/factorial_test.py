97b919dde7f26da3fa08f943c23533a261bcb62d
Leeroyc710 <Leeroy.chiweshe@academytrainee.com>
2019-11-27 10:14:37 +0000

    jenkinsfile change

--- test/factorial_test.py
+++ test/factorial_test.py
@@ -1,5 +1,14 @@
 import pytest
-from factorial_function import factorial1
+import factorial
 
-def test_factorial1():
-      assert factorial1.factorial(5)==120 
+def test_factorial():
+      assert factorial.factorial(5)==120 
+
+def test_big():
+      assert factorial.factorial(13) ==6227020800
+
+def test_bigger():
+      assert factorial.factorial(20)==2432902008176640000
+
+def test_superbig():
+      assert factorial.factorial(30)==265252859812191058636308480000000
